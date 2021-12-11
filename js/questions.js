5c4a6de66a58cb5a8489e79322cffb8acb968ff6
Mohamed Irfan P <70101308+MDI-YT@users.noreply.github.com>
2021-12-11 21:55:05 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "The 16th century was an age of great ______ exploration?",
+  answer: "land",
+  options: [
+    "cosmic",
+    "land",
+    "mental",
+    "common man"
+  ]
+},
+  {
+  numb: 2,
+  question: "Magellan lost the favor of the king of Portugal when he became involved in a political ________?",
+  answer: "entanglement",
+  options: [
+    "entanglement",
+    "discussion",
+    "negotiation",
+    "problem"
+  ]
+},
+  {
+  numb: 3,
+  question: "Four of the ships sought a passage along a southern ______?",
+  answer: "body of land with water on three sides",
+  options: [
+    "coast",
+    "inland",
+    "body of land with water on three sides",
+    "border"
+  ]
+},
+  {
+  numb: 4,
+  question: "The passage was found near 50 degrees S of ________?",
+  answer: "The equator",
+  options: [
+    "Greenwich",
+    "The equator",
+    "Spain",
+    "Portugal"
+  ]
+},
+  {
+  numb: 5,
+  question: "In the spring of 1521, the ships crossed the _______ now called the International Date Line?",
+  answer: "imaginary circle passing through the poles",
+  options: [
+    "imaginary circle passing through the poles",
+    "imaginary line parallel to the equator",
+    "area",
+    "land mass"
+  ]
+},
+];
